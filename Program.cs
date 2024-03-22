7436acee82f7b1e6e3658d12b5ecb7242a670625
Bstrinic <BogdanStrinic003@gmail.com>
2024-03-22 12:02:31 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_8___CPRG_211_F
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
